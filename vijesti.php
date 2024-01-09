e6b1ad3fd963c3d5ac10818ed7eace22ac609fd2
dominikduracic <156125357+dominikduracic@users.noreply.github.com>
2024-01-09 21:17:45 +0100

    Add files via upload

--- vijesti.php
+++ vijesti.php
@@ -63,29 +63,29 @@ $isLoggedIn = isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true;
     <main>
 		<h1>VIJESTI</h1>
 		<div class="vijesti">
-			<a href="vijesti-5.html"><img src="vijesti/vijesti-5.jpg" alt="Španjolska - stižemo!" title="Španjolska - stižemo!"></a>
+			<a href="vijesti-5.php"><img src="vijesti/vijesti-5.jpg" alt="Španjolska - stižemo!" title="Španjolska - stižemo!"></a>
 			<h2>Španjolska - stižemo!</h2>
-			<p>Obucite se laganije jer naša nova hit ljetna destinacija je Španjolska! <a href="vijesti-5.html">Više ...</a></p>
+			<p>Obucite se laganije jer naša nova hit ljetna destinacija je Španjolska! <a href="vijesti-5.php">Više ...</a></p>
 			<p><time datetime="2023-1-2">2. siječnja 2023.</time></p>
 			<hr>
-			<a href="vijesti-4.html"><img src="vijesti/vijesti-4.jpg" alt="Zima dolazi" title="Zima dolazi"></a>
+			<a href="vijesti-4.php"><img src="vijesti/vijesti-4.jpg" alt="Zima dolazi" title="Zima dolazi"></a>
 			<h2>Rusija, Sibir</h2>
-			<p>Naša nova zimska destinacija je Rusija! Sibir! Obucite se toplije! <a href="vijesti-4.html">Više ...</a></p>
+			<p>Naša nova zimska destinacija je Rusija! Sibir! Obucite se toplije! <a href="vijesti-4.php">Više ...</a></p>
 			<p><time datetime="2022-12-22">22. prosinca 2022.</time></p>
 			<hr>
-			<a href="vijesti-3.html"><img src="vijesti/vijesti-3.jpg" alt="Idemo u Portugal" title="Idemo u Portugal"></a>
+			<a href="vijesti-3.php"><img src="vijesti/vijesti-3.jpg" alt="Idemo u Portugal" title="Idemo u Portugal"></a>
 			<h2>Idemo u Portugal</h2>
-			<p>Naša nova eurospka destinacija je Lisabon, Portugal! Pripremite se jer - polijećemo! <a href="vijesti-3.html">Više ...</a></p>
+			<p>Naša nova eurospka destinacija je Lisabon, Portugal! Pripremite se jer - polijećemo! <a href="vijesti-3.php">Više ...</a></p>
 			<p><time datetime="2022-11-21">21. studenog 2022.</time></p>
 			<hr>
-			<a href="vijesti-2.html"><img src="vijesti/vijesti-2.jpg" alt="Hotel Bojan d.o.o." title="Hotel Bojan d.o.o."></a>
+			<a href="vijesti-2.php"><img src="vijesti/vijesti-2.jpg" alt="Hotel Bojan d.o.o." title="Hotel Bojan d.o.o."></a>
 			<h2>Sjajne vijesti! Pokrenuli smo suradnju s hotelom Bojan d.o.o.</h2>
-			<p>Od sada naši klijenti imaju pravo na 20 posto popusta na noćenje u hotelima Bojan! <a href="vijesti-2.html">Više ...</a></p>
+			<p>Od sada naši klijenti imaju pravo na 20 posto popusta na noćenje u hotelima Bojan! <a href="vijesti-2.php">Više ...</a></p>
 			<p><time datetime="2022.-9-12">9. rujna 2022.</time></p>
 			<hr>
-			<a href="vijesti-1.html"><img src="vijesti/vijesti-1.jpg" alt="Doviđenja, Kanada!" title="Doviđenja, Kanada"></a>
+			<a href="vijesti-1.php"><img src="vijesti/vijesti-1.jpg" alt="Doviđenja, Kanada!" title="Doviđenja, Kanada"></a>
 			<h2>Više ne prodajemo avio karte za Kanadu</h2>
-			<p>Utjecajem nepravilnosti na tržištu nažalost avio karte za Kanadu više nisu u našoj ponudi. <a href="vijesti-1.html">Više ...</a></p>
+			<p>Utjecajem nepravilnosti na tržištu nažalost avio karte za Kanadu više nisu u našoj ponudi. <a href="vijesti-1.php">Više ...</a></p>
 			<p><time datetime="2022-8-8">8. kolovoza 2022.</time></p>
 			<hr>
 		</div>
